fae74c3a6cb26612af09c82b782d125a80f7c010
S-Ivanov <s.v.ivanov@bk.ru>
2017-01-26 00:46:42 +0300

    Добавлен каркас COM-загрузчика

---
+++ Com1For1C/IDataLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Com1For1C
+{
+    /// <summary>
+    /// Интерфейс COM-загрузчика данных
+    /// </summary>
+    [Guid("AC2275BD-D8E9-4EC5-988C-3BCE4E1E92BE")]
+    public interface IDataLoader
+    {
+
+    }
+}
---
+++ Com1For1C/Readme.txt
@@ -0,0 +1,2 @@
+﻿Быстрое создание Внешних Компонент на C#. Примеры использования Глобального Контекста, IAsyncEvent, IExtWndsSupport, WinForms и WPF
+http://tf21.ru/public/457898/
